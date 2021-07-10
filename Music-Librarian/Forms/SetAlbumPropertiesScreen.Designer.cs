0d767ca603659071080cbd9b1d8de2511decaee5
Ruben Rodrigues <ruben2000@gmail.com>
2021-07-10 21:44:18 +0100

    Add Missing bits to album properties screen design

--- Music-Librarian/Forms/SetAlbumPropertiesScreen.Designer.cs
+++ Music-Librarian/Forms/SetAlbumPropertiesScreen.Designer.cs
@@ -37,6 +37,11 @@ namespace Forms
 			this.CheckBoxSelectAll = new System.Windows.Forms.CheckBox();
 			this.ButtonChooseSelectWorkout = new System.Windows.Forms.Button();
 			this.ComboBoxWorkouts = new System.Windows.Forms.ComboBox();
+			this.ButtonAllAlbums = new System.Windows.Forms.Button();
+			this.LabelLeewayType = new System.Windows.Forms.Label();
+			this.RadioButtonBoth = new System.Windows.Forms.RadioButton();
+			this.RadioButtonOver = new System.Windows.Forms.RadioButton();
+			this.RadioButtonUnder = new System.Windows.Forms.RadioButton();
 			this.SuspendLayout();
 			// 
 			// ButtonBack
@@ -120,7 +125,7 @@ namespace Forms
 			// CheckBoxSelectAll
 			// 
 			this.CheckBoxSelectAll.AutoSize = true;
-			this.CheckBoxSelectAll.Location = new System.Drawing.Point(19, 112);
+			this.CheckBoxSelectAll.Location = new System.Drawing.Point(25, 238);
 			this.CheckBoxSelectAll.Name = "CheckBoxSelectAll";
 			this.CheckBoxSelectAll.Size = new System.Drawing.Size(97, 21);
 			this.CheckBoxSelectAll.TabIndex = 22;
@@ -160,10 +165,75 @@ namespace Forms
 			this.ComboBoxWorkouts.Size = new System.Drawing.Size(312, 24);
 			this.ComboBoxWorkouts.TabIndex = 24;
 			// 
+			// ButtonAllAlbums
+			// 
+			this.ButtonAllAlbums.AutoSize = true;
+			this.ButtonAllAlbums.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ButtonAllAlbums.BackColor = System.Drawing.Color.Transparent;
+			this.ButtonAllAlbums.FlatAppearance.BorderSize = 0;
+			this.ButtonAllAlbums.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+			this.ButtonAllAlbums.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+			this.ButtonAllAlbums.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.ButtonAllAlbums.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.ButtonAllAlbums.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.ButtonAllAlbums.Location = new System.Drawing.Point(23, 205);
+			this.ButtonAllAlbums.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			this.ButtonAllAlbums.Name = "ButtonAllAlbums";
+			this.ButtonAllAlbums.Size = new System.Drawing.Size(99, 27);
+			this.ButtonAllAlbums.TabIndex = 25;
+			this.ButtonAllAlbums.Text = "All Albums";
+			this.ButtonAllAlbums.UseVisualStyleBackColor = false;
+			// 
+			// LabelLeewayType
+			// 
+			this.LabelLeewayType.AutoSize = true;
+			this.LabelLeewayType.Location = new System.Drawing.Point(23, 77);
+			this.LabelLeewayType.Name = "LabelLeewayType";
+			this.LabelLeewayType.Size = new System.Drawing.Size(110, 17);
+			this.LabelLeewayType.TabIndex = 26;
+			this.LabelLeewayType.Text = "Leeway Type";
+			// 
+			// RadioButtonBoth
+			// 
+			this.RadioButtonBoth.AutoSize = true;
+			this.RadioButtonBoth.Checked = true;
+			this.RadioButtonBoth.Location = new System.Drawing.Point(4, 112);
+			this.RadioButtonBoth.Name = "RadioButtonBoth";
+			this.RadioButtonBoth.Size = new System.Drawing.Size(63, 21);
+			this.RadioButtonBoth.TabIndex = 27;
+			this.RadioButtonBoth.TabStop = true;
+			this.RadioButtonBoth.Text = "Both";
+			this.RadioButtonBoth.UseVisualStyleBackColor = true;
+			// 
+			// RadioButtonOver
+			// 
+			this.RadioButtonOver.AutoSize = true;
+			this.RadioButtonOver.Location = new System.Drawing.Point(4, 139);
+			this.RadioButtonOver.Name = "RadioButtonOver";
+			this.RadioButtonOver.Size = new System.Drawing.Size(63, 21);
+			this.RadioButtonOver.TabIndex = 28;
+			this.RadioButtonOver.Text = "Over";
+			this.RadioButtonOver.UseVisualStyleBackColor = true;
+			// 
+			// RadioButtonUnder
+			// 
+			this.RadioButtonUnder.AutoSize = true;
+			this.RadioButtonUnder.Location = new System.Drawing.Point(4, 166);
+			this.RadioButtonUnder.Name = "RadioButtonUnder";
+			this.RadioButtonUnder.Size = new System.Drawing.Size(73, 21);
+			this.RadioButtonUnder.TabIndex = 29;
+			this.RadioButtonUnder.Text = "Under";
+			this.RadioButtonUnder.UseVisualStyleBackColor = true;
+			// 
 			// SetAlbumPropertiesScreen
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.RadioButtonUnder);
+			this.Controls.Add(this.RadioButtonOver);
+			this.Controls.Add(this.RadioButtonBoth);
+			this.Controls.Add(this.LabelLeewayType);
+			this.Controls.Add(this.ButtonAllAlbums);
 			this.Controls.Add(this.ComboBoxWorkouts);
 			this.Controls.Add(this.ButtonChooseSelectWorkout);
 			this.Controls.Add(this.CheckBoxSelectAll);
@@ -182,6 +252,11 @@ namespace Forms
 			this.Controls.SetChildIndex(this.CheckBoxSelectAll, 0);
 			this.Controls.SetChildIndex(this.ButtonChooseSelectWorkout, 0);
 			this.Controls.SetChildIndex(this.ComboBoxWorkouts, 0);
+			this.Controls.SetChildIndex(this.ButtonAllAlbums, 0);
+			this.Controls.SetChildIndex(this.LabelLeewayType, 0);
+			this.Controls.SetChildIndex(this.RadioButtonBoth, 0);
+			this.Controls.SetChildIndex(this.RadioButtonOver, 0);
+			this.Controls.SetChildIndex(this.RadioButtonUnder, 0);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -197,5 +272,10 @@ namespace Forms
 		private System.Windows.Forms.CheckBox CheckBoxSelectAll;
 		private System.Windows.Forms.Button ButtonChooseSelectWorkout;
 		private System.Windows.Forms.ComboBox ComboBoxWorkouts;
+		private System.Windows.Forms.Button ButtonAllAlbums;
+		private System.Windows.Forms.Label LabelLeewayType;
+		private System.Windows.Forms.RadioButton RadioButtonBoth;
+		private System.Windows.Forms.RadioButton RadioButtonOver;
+		private System.Windows.Forms.RadioButton RadioButtonUnder;
 	}
 }
